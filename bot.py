23e1144cf8d2a465fc0e62c97fbc985d948dabdb
VictorRodas99 <victormatiasrodas100@gmail.com>
2021-01-31 12:09:27 -0300

    Conectar el convertidor al bot de telegram, hecho.

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import telebot as tb
 import requests
 import shutil
+import main
 
 API_TOKEN = "1507843069:AAEOcU9ulclTOjSZmzhGEk4znlTguiqT9Ns"
 bot = tb.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def handle_command(message):
-    bot.reply_to(message, "Hola, bienvenido!")
+    bot.reply_to(message, "Hola! soy un bot que se encarga de convertir toda imagen en texto, si es posible :)")
 
 @bot.message_handler(func=lambda message: True)
 def handle_all_message(message):
@@ -15,15 +16,17 @@ def handle_all_message(message):
 
 @bot.message_handler(content_types=['photo'])
 def handle_all_media(message):
-    #print(message)
+    print("Recibiendo")
     file_id = message.json["photo"][0]["file_id"]
-    bot.reply_to(message, "Recibido" + str(file_id))
     file_info = bot.get_file(file_id) 
     print(file_info.file_path) 
     file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(API_TOKEN, file_info.file_path))
     print(file)
+    main.mkdir()
     if file.status_code == 200:
-        with open("ggg.jpg", "wb") as f:
+        with open("images/imagen.jpg", "wb") as f:
             f.write(file.content)
+
+    bot.reply_to(message, "Texto: \n" + main.convertidor('images/imagen.jpg'))
 
 bot.polling()
--- main.py
+++ main.py
@@ -1,32 +1,22 @@
-# https://stackoverflow.com/questions/33401767/importerror-no-module-named-pytesseract/33402041
-# https://github.com/eternnoir/pyTelegramBotAPI
 try:
     import Image
 except ImportError:
     from PIL import Image
-import datetime
-from pytesseract import *
+import pytesseract
 import os
+import errno
 
+def mkdir():
+    try:
+        os.mkdir('images')
+        os.mkdir('text')
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
 
-def entrada(lista_files):
-    name = input("Introduce el nombre con la extension de la imagen: ")
-    if not name in lista_files:
-        entrada(lista_files)
-    text = pytesseract.image_to_string(Image.open(name))
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+def convertidor(path_name):
+    text = pytesseract.image_to_string(Image.open(path_name))
     print(text)
-    # tomamos solo el nombre sin la extension
-    name_of_file = name.split(".")[0]+".txt"
-    with open(name_of_file, 'w') as arch:
-        arch.writelines("Traducido de imagen a texto en fecha: " +
-                        str(datetime.datetime.now()) + "\n")
-        arch.writelines(text)
-    print("Fin")
-
-
-if __name__ == '__main__':
-    print("--------------Archivos disponibles para convertir------------")
-    lista_files = os.listdir(os.getcwd())
-    print(lista_files)
-    entrada(lista_files)
-    print(lista_files)
+    return text
